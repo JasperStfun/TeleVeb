cac60b45e665f7dbbdec2d180da3c5f7dbbaa602
Konstantin L <kosto55@mail.ru>
2021-06-15 14:40:40 +0300

    доделана бд, установленна админка

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# TeleVeb
+# TeleVeb - ВЕБ версия чата телеграм.
+
+Приложение, для чата в веб-браузере в стиле телеграм.
+
+# Установка
+## Windows:
+
+Скачать проект с github:
+```
+git clone https://github.com/JasperStfun/TeleVeb.git
+```
+Создать виртуальное окружение:
+```
+python -m venv env
+```
+Войти в виртуальное окружение:
+```
+.\env\Scripts\activate
+```
+Установить зависимости:
+```
+pip install -r requirements.txt
+```
+## MacOS/Linux:
+
+Скачать проект с github:
+```
+git clone https://github.com/JasperStfun/TeleVeb.git
+```
+Создать виртуальное окружение:
+```
+python3 -m venv env
+```
+Войти в виртуальное окружение:
+```
+suorce env/bin/activate
+```
+Установить зависимости:
+```
+pip install -r requirements.txt
+```
+
+# Запуск
+Указать flask файл приложения:
+```
+FLASK_APP="televeb.py"
+```
+Запустить flask:
+```
+flask run
+```
--- app/model.py
+++ app/model.py
@@ -1,27 +1,32 @@
+from datetime import datetime
 from app import db
 
 
 class User(db.Model):
-    user = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(21), nullable=False, unique=True)
     email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(28), nullable=False)
+    role = db.Column(db.String(10))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
     def __repr__(self):
-        return f'<User {self.nickname}>'
-
-
-class UserPicture(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
-    pictures = db.Column(db.String)
+        return f'<User: {self.nickname} id: {self.user_id}>'
 
 
 class Message(db.Model):
     message_id = db.Column(db.Integer, primary_key=True)
-    send_user = db.Column(db.Integer)
-    rec_user = db.Column(db.Integer)
+    send_user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    rec_user = db.Column(db.Integer, db.ForeignKey('user.user_id'))
     content = db.Column(db.Text, nullable=True)
-    published = db.Column(db.DateTime, nullable=False)
+    published = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+
+class UserPicture(db.Model):
+    user_id_pic = db.Column(db.Integer, db.ForeignKey('user.user_id'), primary_key=True)
+    pictures = db.Column(db.String)
+
 
 class Chat(db.Model):
     chat_id = db.Column(db.Integer, primary_key=True)
-    user = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
