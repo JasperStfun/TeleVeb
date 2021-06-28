c6f5a1dfa0bc6a65feffc79355b8a09615a8451c
Konstantin L <kosto55@mail.ru>
2021-06-28 12:19:53 +0300

    Примитивный чат и список юзеров

--- app/__init__.py
+++ app/__init__.py
@@ -4,19 +4,24 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_admin import Admin
 from flask_login import LoginManager
+from sqlalchemy import MetaData
+
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(app, metadata=metadata)
+migrate = Migrate(app, db, render_as_batch=True)
 admin = Admin(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
-with app.app_context():
-    if db.engine.url.drivername == 'sqlite':
-        migrate.init_app(app, db, render_as_batch=True)
-    else:
-        migrate.init_app(app, db)
 
-from app import routes, model
+from app import routes, model, migrate
--- app/model.py
+++ app/model.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+from enum import unique
+
+from sqlalchemy.orm import relationship
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -11,11 +14,12 @@ def load_user(id):
 
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(21), nullable=False, unique=True)
     email = db.Column(db.String(120), nullable=False, unique=True)
     password_hash = db.Column(db.String(28))
-    role = db.Column(db.String(10))
+    role = db.Column(db.String(10), default='user')
     message = db.relationship('Message', backref='author', lazy='dynamic')
 
     def set_password(self, password):
@@ -25,22 +29,28 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return f'<User: {self.nickname} id: {self.id}>'
+        return f'<User: {self.username} id: {self.id}>'
 
 
 class Message(db.Model):
+    __tablename__ = 'message'
     message_id = db.Column(db.Integer, primary_key=True)
-    send_user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    send_user_name = db.Column(db.String, db.ForeignKey('user.username'))
     content = db.Column(db.Text, nullable=True)
     published = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
+    def __repr__(self):
+        return f'{self.send_user_name}: {self.content}'
+
 
 class UserPicture(db.Model):
+    __tablename__ = 'user_picture'
     user_id_pic = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
     pictures = db.Column(db.String)
 
 
 class Chat(db.Model):
+    __tablename__ = 'chat'
     chat_id = db.Column(db.Integer, primary_key=True)
     user_1_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     user_2_id = db.Column(db.Integer, db.ForeignKey('user.id'))
