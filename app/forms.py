ebf49a25b1fac825b4473c90aa7e6da7e29cc28a
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-06-29 11:58:01 +0300

    add chat form

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, PasswordField, StringField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from wtforms.fields.simple import TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.validators import ValidationError, Length
 from app.model import User
 from wtforms.fields.html5 import EmailField
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=4, max=16)])
+    password = PasswordField('Password', validators=[DataRequired(),
+                             Length(min=4, max=16)])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
@@ -15,7 +18,8 @@ class LoginForm(FlaskForm):
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = EmailField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=4, max=16)])
+    password = PasswordField('Password', validators=[DataRequired(),
+                             Length(min=4, max=16)])
     password_2 = PasswordField(
         'Repeat password', validators=[DataRequired(), EqualTo('password')]
     )
@@ -30,3 +34,8 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email adress.')
+
+
+class ChatForm(FlaskForm):
+    user_message = TextAreaField('New Message', validators=[DataRequired()])
+    submit = SubmitField('Send')
