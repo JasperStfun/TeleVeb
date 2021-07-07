d8ecd80dd9a7e9d68f5df6e295be73f3284233cb
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-07-07 16:29:07 +0300

    update @socketio.on

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from enum import unique
+import uuid
 from flask import render_template, flash, redirect, request, url_for
 from app import app, db, socketio
 from app.forms import ChatForm, EmailEditForm, LoginForm, ProfileForm, RegistrationForm, UsernameEditForm
@@ -8,6 +8,7 @@ from flask_login import current_user, login_user, login_required, logout_user
 from app.model import Message, User, Chat, UserArchive
 from werkzeug.urls import url_parse
 from flask_socketio import emit, join_room
+from datetime import datetime
 
 
 @app.route('/')
@@ -68,12 +69,12 @@ def welcome_to_chat():
             search_result = User.query.filter(User.username == content).first()
             if search_result is not None:
                 search_result = search_result.username
-                return render_template('welcome_to_chat.html',users=users, search_form=search_form,
-                                    search_result=search_result)
+                return render_template('welcome_to_chat.html', users=users, search_form=search_form,
+                                       search_result=search_result)
             else:
                 flash('User not found')
                 return render_template('welcome_to_chat.html',
-                                   users=users, search_form=search_form)
+                                       users=users, search_form=search_form)
         else:
             return render_template('welcome_to_chat.html',
                                    users=users, search_form=search_form)
@@ -86,54 +87,51 @@ def chat(pk):
     if current_user.is_authenticated:
         user_1 = current_user
         user_2 = User.query.filter(User.id == pk).first_or_404()
-        chat_check = Chat.query.filter(
+        chat_existence = Chat.query.filter(
             (Chat.user_1_id == user_1.id) | (Chat.user_2_id == user_1.id)
         ).filter(
             (Chat.user_1_id == user_2.id) | (Chat.user_2_id == user_2.id)
         ).first()
-        if chat_check is None:
-            chat = create_chat(user_1, user_2)
-            chat = chat.id
+        if chat_existence is None:
+            chat = create_chat(user_1, user_2, uuid.uuid4())
+            chat_id = chat.id
         else:
-            chat = chat_check.id
+            chat_id = chat_existence.id
         chat_form = ChatForm()
-        if chat_form.validate_on_submit():
-            content = chat_form.user_message.data
-            message = Message(send_user_id=user_1.id, content=content,
-                              message_chat_id=chat)
-            db.session.add(message)
-            db.session.commit()
-            return redirect(url_for('chat', pk=pk))
-        all_messages = Message.query.filter(Message.message_chat_id == chat).all()
-        return render_template('chat.html', user_1=user_1, user_2=user_2, chat=chat,
+        all_messages = Message.query.filter(Message.message_chat_id == chat_id).all()
+        return render_template('chat.html', user_1=user_1, user_2=user_2, chat_id=chat_id,
                                chat_form=chat_form, all_messages=all_messages)
     flash('Login or register')
     return redirect(url_for('login'))
 
 
-def create_chat(user_1, user_2):
-    chat = Chat(user_1_id=user_1.id, user_2_id=user_2.id)
+def create_chat(user_1, user_2, uuid):
+    chat = Chat(user_1_id=user_1.id, user_2_id=user_2.id, unique_number=uuid)
     db.session.add(chat)
     db.session.commit()
     return chat
 
+
 @socketio.on('send message')
-def handle_message(message, chat):
+def handle_message(message, chat_id):
     if current_user.is_authenticated:
-       chat_check = Chat.query.filter(Chat.id == chat).first_or_404()
-       message = str(message); chat = str(chat)  
-       send_user = int(current_user.id)
-       content = Message(content=message, message_chat_id=chat, send_user_id=send_user)
-       db.session.add(content)
-       db.session.commit()
-       emit('display message', message, room=str(chat_check.id))
+        chat_existence = Chat.query.filter(Chat.id == chat_id).first_or_404()
+        message = str(message)
+        dt_now = datetime.now().strftime('%d.%m.%Y %H:%M')
+        send_user = current_user.id
+        content = Message(content=message, message_chat_id=chat_id,
+                          send_user_id=send_user, published=dt_now)
+        db.session.add(content)
+        db.session.commit()
+        message_info = f'{dt_now} {current_user.username}: {message}'
+        emit('display message', message_info, room=chat_existence.unique_number)
 
 
 @socketio.on('join')
-def join(chat):
-    chat_check = Chat.query.filter(Chat.id == chat).first_or_404()
-    join_room(str(chat_check.id))
-
+def join(chat_id):
+    chat_existence = Chat.query.filter(Chat.id == chat_id).first_or_404()
+    room = chat_existence.unique_number
+    join_room(room)
 
 
 @app.route('/profile/<pk>', methods=['GET', 'POST'])
@@ -147,11 +145,12 @@ def user_profile(pk):
     flash('Login or register')
     return redirect(url_for('login'))
 
+
 @app.route('/edit_profile/<pk>', methods=['GET', 'POST'])
 def edit_profile(pk):
     if current_user.is_authenticated:
         username_edit_form = UsernameEditForm()
-        email_edit_form= EmailEditForm()
+        email_edit_form = EmailEditForm()
         if username_edit_form.validate_on_submit():
             edit_username(current_user, username_edit_form, pk)
         if email_edit_form.validate_on_submit():
@@ -165,8 +164,9 @@ def edit_profile(pk):
     flash('Login or register')
     return redirect(url_for('login'))
 
+
 def edit_username(current_user, username_edit_form, pk):
-    user = User.query.filter(User.id==current_user.id).first()
+    user = User.query.filter(User.id == current_user.id).first()
     user_archive = UserArchive(username=username_edit_form.username.data)
     user.username = username_edit_form.username.data
     db.session.add(user_archive)
@@ -174,8 +174,9 @@ def edit_username(current_user, username_edit_form, pk):
     flash('You have successfully changed your username!')
     return redirect(url_for('edit_profile', pk=pk))
 
+
 def edit_email(current_user, email_edit_form, pk):
-    user = User.query.filter(User.id==current_user.id).first()
+    user = User.query.filter(User.id == current_user.id).first()
     user.email = email_edit_form.email.data
     db.session.commit()
     flash('You have successfully changed your email!')
